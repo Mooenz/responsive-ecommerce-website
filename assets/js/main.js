0bb1cfed875f37445e4b2b6cac7667ddd0bdaf14
mooenz <joss92821@hotmail.com>
2022-04-13 18:21:08 -0500

    add javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
-/*===== MENU SHOW =====*/ 
+/*===== MENU SHOW =====*/
+const toggle = document.getElementById('nav-toggle'),
+  nav = document.getElementById('nav-menu');
 
+const showMenu = () => {
+  nav.classList.toggle('show');
+};
+
+toggle.addEventListener('click', showMenu, false);
 
 /*===== REMOVE MENU MOBILE =====*/
+const navLinkNode = document.querySelectorAll('.nav__link');
+const navLink = [...navLinkNode];
+
+navLink.map((item) => {
+  const linkAction = () => {
+    navLink.map((item2) => {
+      item2.classList.remove('active');
+    });
+
+    // Add avtice
+    item.classList.add('active');
+
+    // Remove menu mobile
+    nav.classList.remove("show")
+  };
+
+  item.addEventListener('click', linkAction, false);
+});
